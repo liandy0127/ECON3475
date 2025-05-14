bacfd6bb03013897a5e0b8fd5f955a792ff55e98
Andy Li <sl2535@cornell.edu>
2025-05-14 10:44:46 -0400

    website

--- general_scrolling.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener('wheel', (event) => {
-    document.documentElement.scrollLeft += event.deltaY; //scroll left if scrolling down
-    document.documentElement.scrollLeft += event.deltaX; //scroll left if scrolling left
-});
